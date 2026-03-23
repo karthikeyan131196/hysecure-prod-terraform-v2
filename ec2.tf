787c3b1e2db0c5cf0582a848dd7e1dabbd19a19d
karthikeyan131196 <karthikeyan131196@gmail.com>
2026-03-23 22:47:51 +0530

    update

--- ec2.tf
+++ ec2.tf
@@ -4,6 +4,7 @@ resource "tls_private_key" "hysecure_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
+
 resource "local_file" "hysecure_pem" {
   content         = tls_private_key.hysecure_key.private_key_pem
   filename        = "${path.module}/${var.project_name}-key.pem"
@@ -17,14 +18,14 @@ resource "aws_key_pair" "hysecure_key" {
   public_key = tls_private_key.hysecure_key.public_key_openssh
 }
 
-#AWS Ec2
+# AWS EC2
 
 locals {
   use_source_ami = var.aws_region == "ap-south-1"
 }
 
 resource "aws_ami_copy" "hysecure" {
-  count = local.use_source_ami ? 0 : 1
+  for_each = local.use_source_ami ? {} : { copy = true }
 
   name              = "hysecure-${var.aws_region}"
   source_ami_id     = var.source_ami_id
@@ -37,18 +38,20 @@ resource "aws_ami_copy" "hysecure" {
   }
 }
 
-#Avalilable zone
+# Available zone
 locals {
   subnet_by_az = {
     "${var.aws_region}a" = aws_subnet.az1a.id
     "${var.aws_region}b" = aws_subnet.az1b.id
   }
+
+  final_ami_id = local.use_source_ami ? var.source_ami_id : aws_ami_copy.hysecure["copy"].id
 }
 
 resource "aws_instance" "nodes" {
   for_each = var.instance_az_map
-
-  ami                         = local.use_source_ami ? var.source_ami_id : aws_ami_copy.hysecure[0].id
+  
+  ami                         = local.final_ami_id
   instance_type               = var.instance_type
   subnet_id                   = local.subnet_by_az[each.value]
   vpc_security_group_ids      = [aws_security_group.hysecure_sg.id]
